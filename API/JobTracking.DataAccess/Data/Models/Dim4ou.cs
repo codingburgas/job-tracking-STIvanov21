3e085d293a6eb8e0d8a80bc22bc3b1503512db68
Стилиян Танев Иванов <STIvanov21@codingburgas.bg>
2025-05-09 09:56:14 +0300

    Add models

---
+++ API/JobTracking.DataAccess/Data/Models/Dim4ou.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JobTracking.DataAccess.Data.Base;
+
+namespace JobTracking.DataAccess.Data.Models;
+
+public class Dim4ou : IEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public required string CreatedBy { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    
+    public ICollection<KriskoBeats> KriskoBeats { get; set; } = new List<KriskoBeats>();
+}
---
+++ API/JobTracking.DataAccess/Data/Models/KriskoBeats.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JobTracking.DataAccess.Data.Base;
+
+namespace JobTracking.DataAccess.Data.Models;
+
+public class KriskoBeats : IEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public required string CreatedBy { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    
+    [Required]
+    public int Dim4ouID {get; set;}
+    public virtual Dim4ou Dim4ou { get; set; }
+}
