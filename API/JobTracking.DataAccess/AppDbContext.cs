d00ec5416351e0a854328eb4547e5f92c8859dc1
Стилиян Танев Иванов <STIvanov21@codingburgas.bg>
2025-05-16 10:00:12 +0300

    Add DbContext

---
+++ API/JobTracking.DataAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JobTracking.DataAccess.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracking.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Dim4ou> Dim4ou { get; set; }
+    public DbSet<KriskoBeats> KriskoBeats { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=JobTrackingDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ API/JobTracking.DataAccess/Migrations/20250516065056_Initial Migration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTracking.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dim4ou",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dim4ou", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KriskoBeats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dim4ouID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KriskoBeats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_KriskoBeats_Dim4ou_Dim4ouID",
+                        column: x => x.Dim4ouID,
+                        principalTable: "Dim4ou",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KriskoBeats_Dim4ouID",
+                table: "KriskoBeats",
+                column: "Dim4ouID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KriskoBeats");
+
+            migrationBuilder.DropTable(
+                name: "Dim4ou");
+        }
+    }
+}
