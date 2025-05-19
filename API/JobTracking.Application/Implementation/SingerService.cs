d998466cb0e9b17cf99dd15d606f52f698fd092e
Стилиян Танев Иванов <STIvanov21@codingburgas.bg>
2025-05-19 10:14:18 +0300

    Add user implementation

---
+++ API/JobTracking.Application/Implementation/SingerService.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace JobTracking.Application.Implementation;
+
+public class SingerService : IsingerService
+{
+    protected DependencyProvider Provider { get; set; }
+
+    public SingerService(DependencyProvider provider)
+    {
+        Provider = provider;
+    }
+
+    public Task<IQueryable<Singer>> GetAllSingers(int page, int pageCount)
+    {
+        var query = Provider.Db.Singers
+            .Skip(page - 1 * pageCount)
+            .Take(PageCount);
+        
+        query = query.Where(s =? s.Name == "preslava");
+
+        var result = query.toList();
+        
+        
+    }
+}
