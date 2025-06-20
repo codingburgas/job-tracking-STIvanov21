d35c6c295c048011f19723333a516f4f667d69fd
STIvanov21 <STIvanov21@codingburgas.bg>
2025-06-20 09:31:40 +0300

    Add Project

--- WEB/src/app/app.component.ts
+++ WEB/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, TranslateModule],
+  imports: [CommonModule, RouterOutlet, TranslateModule, NavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     standalone: true,
--- WEB/src/app/app.routes.ts
+++ WEB/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'jobs', component: JobsComponent },
+  { path: '**', redirectTo: '' }
+];
